1f4da019e643fc2a4bd3b545a2396d1d163e6bac
MayeKnight <1284741205@qq.com>
2025-04-10 18:15:29 +0800

    modified:   data_structure_learn/Data_Structure 	new file:   "data_structure_learn/data_structure_program/\347\254\254\344\271\235\347\253\240_\345\233\276/9.3.2_\345\233\276\347\232\204\346\267\261\345\272\246\344\274\230\345\205\210\351\201\215\345\216\206.cpp"

---
+++ data_structure_learn/data_structure_program/第九章_图/9.3.2_图的深度优先遍历.cpp
@@ -0,0 +1,121 @@
+//上一节内容调用
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//构造vertex类
+class Vertex
+{
+public:
+    int val; //顶点的值
+
+    //构造函数
+    Vertex(int value) : val(value) {}
+};
+
+class GraphAdjList
+{
+public:
+    //邻接表，key:顶点, value:该顶点的所有邻接顶点
+    unordered_map<Vertex*, vector<Vertex*>> adjList;  //这里是使用了哈希表，建议结合先前章节理解，adjList本身有多个key，每个key都对应有value
+
+    //在vector中删除指定节点
+    void remove(vector<Vertex*>& vec, Vertex* vet)
+    {
+        for (int i = 0; i < vec.size(); i++)
+        {
+            if (vec[i] == vet)
+            {
+                vec.erase(vec.begin() + i); //这里实质上是将vec[i] == vet对应的实例删除了，所以在对应图示中，左边列表和右边对应"链表"中相应实例均被删除了
+            }
+        }
+    }
+
+    //构造方法(或构造函数)是类的一种特殊方法, 用于初始化类的对象. 即构造方法就是使用类的方法
+    GraphAdjList(const vector<vector<Vertex*>>& edges)
+    {
+        //添加所有顶点和边
+        for (const vector<Vertex*>& edge : edges)
+        {
+            addVertex(edge[0]);
+            addVertex(edge[1]);
+            addEdge(edge[0], edge[1]);
+        }
+    }
+
+    //获取顶点数量
+    int size()
+    {
+        return adjList.size(); //这里是返回了adjList的key数量, 即顶点数量
+    }
+
+    //添加边
+    void addEdge(Vertex* vet1, Vertex* vet2)
+    {
+        if (!adjList.count(vet1) || !adjList.count(vet2) || vet1 == vet2)
+            throw invalid_argument("不存在顶点"); //throw用于抛出异常，invalid_arguement是一个标准库异常类，表示传参无效
+        //添加边 vet1 - vet2
+        adjList[vet1].push_back(vet2);
+        adjList[vet2].push_back(vet1);
+    }
+
+    //删除边
+    void removeEdge(Vertex* vet1, Vertex* vet2)
+    {
+        if (!adjList.count(vet1) || !adjList.count(vet2) || vet1 == vet2)
+            throw invalid_argument("不存在顶点");
+        //删除边 vet1 - vet2
+        remove(adjList[vet1], vet2); // remove(列表，值)函数表示从列表中删去对应值, 这里含义则是删去vet1对应"链表"中的vet2
+        remove(adjList[vet2], vet1);
+    }
+
+    //添加顶点
+    void addVertex(Vertex* vet)
+    {
+        if (adjList.count(vet))  //若已经存在该顶点则直接返回
+            return;
+        //在临界表中添加一个新链表，这里实质上是声明一个新键值对，其中键为该节点，值为一新的空链表
+        adjList[vet] = vector<Vertex*>();
+    }
+
+    //删除顶点
+    void removeVertex(Vertex* vet)
+    {
+        if (!adjList.count(vet))
+            throw invalid_argument("不存在顶点");
+        //在邻接表中删除顶点vet对应的链表
+        adjList.erase(vet);
+        //遍历其他顶点的链表，删除所有包含vet的边
+        for (auto& adj : adjList)
+        {
+            remove(adj.second, vet);
+        }
+    }
+
+};
+
+//本节内容如下
+//深度优先遍历辅助函数
+void dfs(GraphAdjList& graph, unordered_set<Vertex*>& visited, vector<Vertex*>& res, Vertex* vet)
+{
+    res.push_back(vet);
+    visited.emplace(vet);
+    for (Vertex* adjVet : graph.adjList[vet])  //这里的graph.adjList[vet]即身为graphAdjList类的对象graph中所包含adjList成员函数, adjList为哈希表, 故这里adjList[vet]即键vet对应的值, 这里的值即为与顶点'vet'相关联的所有邻接顶点的列表
+    {
+        if (visited.count(adjVet))
+            continue;
+        dfs(graph, visited, res, adjVet);
+    }
+}
+
+//深度优先遍历
+//使用邻接表来表示图, 以便获取指定顶点的所有邻接顶点
+vector<Vertex* > graphDFS(GraphAdjList& graph, Vertex* startVet) //该函数类型为vector<Vertex* >则最后也应该返回这个类型
+{
+    //哈希集合, 用于记录已被访问过的顶点
+    unordered_set<Vertex* > visited;
+    //顶点遍历序列
+    vector<Vertex* > res;
+    dfs(graph, visited, res, startVet);
+    return res;
+}
