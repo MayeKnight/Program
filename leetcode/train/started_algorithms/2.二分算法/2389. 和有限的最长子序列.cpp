c3281dcc04028aa5bf6016e64b0964eadc066180
MayeKnight <1284741205@qq.com>
2026-02-08 18:18:42 +0800

    main

--- leetcode/train/started_algorithms/2.二分算法/2389. 和有限的最长子序列.cpp
+++ leetcode/train/started_algorithms/2.二分算法/2389. 和有限的最长子序列.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+    int Partition(vector<int>& nums, int low, int high) {
+        int pivot = nums[low];
+        while (low < high) {
+            while (low < high && nums[high] >= pivot)
+                high--;
+            nums[low] = nums[high];
+
+            while (low < high && nums[low] <= pivot)
+                low++;
+            nums[high] = nums[low];
+        }
+        nums[low] = pivot;
+        return low;
+    }
+
+    void QuickSort(vector<int>& nums, int low, int high) {
+        if (low < high) {
+            int pivotpos = Partition(nums, low, high);
+            QuickSort(nums, low, pivotpos - 1);
+            QuickSort(nums, pivotpos + 1, high);
+        }
+    }
+
     int lower_bound(vector<int>& nums, int target) {
         int left = 0, mid = 0;
         int right = nums.size() - 1;
@@ -16,9 +39,10 @@ public:
         }
         return left;
     }
+
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         vector<int> res;
-        ranges::sort(nums);
+        QuickSort(nums, 0, nums.size() - 1);
         partial_sum(nums.begin(), nums.end(), nums.begin());  //前缀和原地计算
         for (int i = 0; i < queries.size(); i++) {
             res.push_back(lower_bound(nums, queries[i] + 1) - 1 + 1);
